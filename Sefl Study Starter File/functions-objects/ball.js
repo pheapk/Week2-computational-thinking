5098770f50a1b7f5ffd0e3d4e814dc8d49f3d417
Pheap <khmoping@gmail.com>
2021-10-03 14:27:37 -0700

    added WHILE loop to get objects from array, then create balls based on that object's properties.

--- Sefl Study Starter File/functions-objects/ball.js
+++ Sefl Study Starter File/functions-objects/ball.js
@@ -1,4 +1,11 @@
 var length = data.length;
 
 // you work goes here
-// -----------------------
+// ------------------
+// Sun 10/3/2021
+var counter = 0; 
+while (counter < length) {
+    var ball = data[counter]; // get the ball from the array 
+    create(ball.x,ball.y,ball.color); // create the ball using object's properties from the array
+    counter = counter +  1;
+}
