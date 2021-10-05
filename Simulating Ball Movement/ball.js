07156488ae94d0531a45bd3cef5de2c3a2af1335
Pheap <khmoping@gmail.com>
2021-10-04 22:18:38 -0700

    added position, valocity, ball.style.left

--- Simulating Ball Movement/ball.js
+++ Simulating Ball Movement/ball.js
@@ -1,11 +1,17 @@
 //Set global variable that would contain the position, velocity and the html element "ball"
 var ball = document.getElementById("ball");
 
+var velocity = 100;
+var positionX = 0;
+
 //write a function that can change the position of the html element "ball"
 function moveBall() {
   // two fixed x-axis coordinates (you will use these variable in step 3)
   var Xmin = 0;
   var Xmax = 300;
+    postionX = positionX + velocity;
+    ball.style.left = postionX;
+
 }
 
 // This call the moveBall function every 100ms
