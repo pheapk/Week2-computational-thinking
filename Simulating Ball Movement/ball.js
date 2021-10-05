67dd1e357fed01592717a2c2200d209e94489076
Pheap <khmoping@gmail.com>
2021-10-04 22:36:00 -0700

    adding the movement and bounce on x axis

--- Simulating Ball Movement/ball.js
+++ Simulating Ball Movement/ball.js
@@ -4,14 +4,26 @@ var ball = document.getElementById("ball");
 var velocity = 100;
 var positionX = 0;
 
+var reverse = false;
+
 //write a function that can change the position of the html element "ball"
 function moveBall() {
   // two fixed x-axis coordinates (you will use these variable in step 3)
   var Xmin = 0;
   var Xmax = 300;
-    postionX = positionX + velocity;
-    ball.style.left = postionX;
 
+  if (positionX >= Xmax) {
+    reverse = true;
+  }else{
+    reverse = false;
+  }
+
+  if (reverse == false){
+    positionX = positionX + velocity;
+  }else {
+    positionX = positionX - velocity;
+  }
+  ball.style.left = positionX;
 }
 
 // This call the moveBall function every 100ms
