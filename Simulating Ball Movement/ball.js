eb2be4f21d137ba8752acf8dfd078be07d6f2675
Pheap <khmoping@gmail.com>
2021-10-04 22:03:15 -0700

    simulating ball movement

---
+++ Simulating Ball Movement/ball.js
@@ -0,0 +1,11 @@
+//Set global variable that would contain the position, velocity and the html element "ball"
+
+//write a function that can change the position of the html element "ball"
+function moveBall() {
+  // two fixed x-axis coordinates (you will use these variable in step 3)
+  var Xmin = 0;
+  var Xmax = 300;
+}
+
+// This call the moveBall function every 100ms
+setInterval(moveBall, 100);
